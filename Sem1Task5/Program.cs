0f4f6afb122a07e4a114f9ff3a4932248d9ecb26
Strogyy <xzerks@mail.ru>
2023-01-27 14:59:46 +0500

    s1t5 null 1

--- Sem1Task5/Program.cs
+++ Sem1Task5/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// напишите программу которая на вход принимает одно число а на выходе показывает все целые числа в промежутке от -n до n  
+
+string? numberA = Console.ReadLine();
+
+int numberB = Int.Parse (numberA);
+
+int numberC = (numberB * -1);
+
+string R = string.Empty;
+

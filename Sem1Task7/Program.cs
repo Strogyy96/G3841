c310ac4689349d08abc9d7befd61fd879bcc9a92
Strogyy <xzerks@mail.ru>
2023-01-28 22:00:16 +0500

    s1t7 done

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿// принимаем на входе трехзначное число и на выходе показываем последнюю цифру трехзначного числа
+
+string? numberA = Console.ReadLine();
+// проверяем чтобы данные не были пустыми
+if (numberA != null)
+{
+    // парсим введенное число
+    int numberB = int.Parse(numberA);
+    // остаток от деления на 10 с помощью оператора %
+    int LastDigit = numberB % 10;
+
+    Console.WriteLine(LastDigit);
+}

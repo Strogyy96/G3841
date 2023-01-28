3c138cddf02e1c2249c549f5cd267f7aecd09d6b
Strogyy <xzerks@mail.ru>
2023-01-28 21:42:17 +0500

    s1t5 done

--- Sem1Task5/Program.cs
+++ Sem1Task5/Program.cs
@@ -1,10 +1,22 @@
 ﻿// напишите программу которая на вход принимает одно число а на выходе показывает все целые числа в промежутке от -n до n  
-
+// считываем данные с косоли
 string? numberA = Console.ReadLine();
+// проверяем чтобы данные не были пустыми
+if (numberA!=null)
+{
+// парсим введенное число
+   int numberB = int.Parse (numberA);
+// вводим переменную для отрицательных чисел
+   int numberC = (numberB * -1);
+// добавляем выходные значения
+   string R = string.Empty;
+// цикл вайл 
+   while(numberC < numberB)
+   {
+    R = R + numberC + ",";
+    numberC = numberC + 1;
+   }
+R = R + numberB;
 
-int numberB = Int.Parse (numberA);
-
-int numberC = (numberB * -1);
-
-string R = string.Empty;
-
+Console.WriteLine(R);
+}

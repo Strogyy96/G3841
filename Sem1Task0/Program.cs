c1f3ac5b5d63a73bfdc337330bd0561c92794d11
Strogyy <xzerks@mail.ru>
2023-01-28 22:05:48 +0500

    add solution for task 0,1,3,5,7

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,18 @@
+﻿// считываем данные с консоли
+string? inputNum = Console.ReadLine();
+// проверяем чтобы данные были не пустыми
+if (inputNum != null)
+{
+    // // Парсим введенное число (превращаем строку в число)
+    // int number = int.Parse(inputNum);
+    // // Находим квадрат числа
+    // // int result = number*number;
+    // int result = (int)Math.Pow(number,2);
+
+
+
+    // // Вывоим данные в консоль
+    // Console.WriteLine(result);
+
+    Console.WriteLine(Math.Pow(int.Parse(inputNum),2));
+}
---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+// Проверяем, чтобы данные не были пустыми
+if(inputLine!=null)
+{
+    
+// Вариант 1
+    // string [] dayWeek = new string[7];
+    // dayWeek [0] = "понедельник";
+    // dayWeek [1] = "вторник";
+    // dayWeek [2] = "среда";
+    // dayWeek [3] = "четверг";
+    // dayWeek [4] = "пятница";
+    // dayWeek [5] = "суббота";
+    // dayWeek [6] = "воскресение";
+    
+    // int nameDay = int.Parse(inputLine);
+    // Console.WriteLine(dayWeek[nameDay-1]);
+
+    // Вариант 2
+    int nameDay = int.Parse(inputLine);
+    string dayWeek = string.Empty;
+    switch (nameDay)
+    {
+        case 1: dayWeek = "понедельник"; break;
+        case 2: dayWeek = "вторник"; break;
+        case 3: dayWeek = "среда"; break;
+        case 4: dayWeek = "четверг"; break;
+        case 5: dayWeek = "пятница"; break;
+        case 6: dayWeek = "суббота"; break;
+        case 7: dayWeek = "воскресение"; break;
+        default: dayWeek = "Это не день недели!"; break;
+    }
+    Console.WriteLine(dayWeek);
+}

9a60107d06aecc0e128c866477d2b5965bf255c4
Strogyy <xzerks@mail.ru>
2023-01-28 23:36:09 +0500

    add solution for task 2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// вводим данные 
+string? inputNumLineA = Console.ReadLine();
+
+string? inputNumLineB = Console.ReadLine();
+// проверяем чтобы небыло пустых значений
+if (inputNumLineA!=null&&inputNumLineB!=null)
+// превращаем строки в цело численные значения
+{
+   int numberA = int.Parse(inputNumLineA);
+   int numberB = int.Parse(inputNumLineB);
+   // циклом if else определяем большее число и выводим ответ
+      if (numberA>numberB)
+      {
+        Console.WriteLine("Первое число больше второго!");    
+      }
+      else 
+      {
+        Console.WriteLine("Второе число больше первого!");
+      }
+
+
+
+}
+

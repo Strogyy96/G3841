039d4ec64e7809d740d2b3eff352bfa22c5094d9
Strogyy <xzerks@mail.ru>
2023-02-09 11:56:42 +0500

    add solution for task 10,13,15

---
+++ Sem1Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿// программа получает на вход цифру обозначающую день недели и на выходе отвечает 
+// является этот день выходным или нет
+int day = int.Parse(Console.ReadLine()??"0");
+
+if (day > 7 || day < 1)
+{
+    Console.WriteLine("не день недели");
+}
+else
+{
+    if (day>5)
+    {
+        Console.WriteLine("Выходной");
+
+    }
+    else
+    {
+        Console.WriteLine("рабочий");
+
+    }
+}
---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿// выводит третью цифру заданного числа или сообщает что третьей цифры нет
+
+Console.WriteLine("Введите число: ");
+
+
+
+char[] digit = Console.ReadLine().ToCharArray();
+
+if (char.Length > 3)
+{
+    Console.WriteLine(digit[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
